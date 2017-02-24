13e70d0797fe313cc96e54a1d4781dc451538934
Navleen Gill <navleengill1661@gmail.com>
2017-02-24 14:17:31 -0500

    Added comment header for the C# files

--- COMP1004-W2017-MidTermAssgnment-StudentID/Program.cs
+++ COMP1004-W2017-MidTermAssgnment-StudentID/Program.cs
@@ -1,6 +1,15 @@
 ﻿using System;
 using System.Windows.Forms;
 
+/// <summary>
+/// App name: Character Generator
+/// Author's name: Navleen Kaur Gill
+/// Student ID: 200334989
+/// App Creation Date: February 24, 2017
+/// App Description: This app generates	the	first few pages of an RPG character	and display	the	
+///results of randomly generated abilities and the users selections.
+/// 
+/// </summary>
 namespace COMP1004_W2017_MidTermAssgnment_StudentID
 {
   static class Program
